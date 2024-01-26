8a6fe91edfb19633a011dcbb38adf53fe812db68
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-26 16:29:28 +0100

    data/drinks-fertig

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 
 import './App.css'
-
 import Header2 from './components/header2/Header2'
-
-
+import { Routes, Route } from 'react-router-dom'
+import AddDrink from './assets/pages/addDrink/AddDrink'
 
 function App() {
 
 
   return (
     <>
-    
-    <Header2/> 
+      <Routes>
+      <Route path='/AddDrink' element={<AddDrink />} />
+
+
+
+      </Routes>
+      <Header2 />
     </>
   )
 }
